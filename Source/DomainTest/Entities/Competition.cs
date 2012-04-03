e932a99cfa7cc0f0296f71eb23fe6a07e9b35990
unknown <Henry Lawson@Whitmore.(none)>
2012-04-03 21:21:31 +0530

    Initial project with nuget, nunit and a simple silly test setup

---
+++ Source/DomainTest/Entities/Competition.cs
@@ -0,0 +1,17 @@
+﻿namespace DomainTest.Entities
+{
+    using Domain.Entities;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class CompetitionTest
+    {
+        [Test]
+        public void ShouldHaveNameProperty()
+        {
+            var competition = new Competition { Name = "Brisbane State Championships" };
+            Assert.That(competition.Name, Is.EqualTo(competition.Name));
+        }
+    }
+}
